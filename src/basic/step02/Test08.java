eb5f1fe9aa1c40b67c7ccebd5498fc3f5e901fe3
user <user@DESKTOP-S9IISRV>
2020-07-05 11:27:19 +0900

    Day02 review 0705

--- src/basic/step02/Test08.java
+++ src/basic/step02/Test08.java
@@ -1,4 +1,7 @@
 package basic.step02;
+
+import java.util.Scanner;
+
 /*
  * # ATM[1단계] : 이체하기
  * 1. 이체할 계좌번호를 입력받는다.
@@ -9,19 +12,38 @@ package basic.step02;
  * 			yourMoney + 이체할 금액
  *    이체할 금액  > myMoney 	: 이체불가
  */
-
+//6분
 public class Test08 {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		int myAcc = 1234;
 		int myMoney = 8700;
 
 		int yourAcc = 4321;
 		int yourMoney = 12000;
 
+		System.out.println("이체할 계좌번호 입력: ");
+		int sendAcc = sc.nextInt();
+		if (sendAcc == yourAcc) {
+			System.out.println("이체할 금액 입력: ");
+			int sendMoney = sc.nextInt();
+
+			if (sendMoney <= myMoney) {
+				myMoney = myMoney - sendMoney;
+				yourMoney = yourMoney + sendMoney;
+				
+				System.out.println("이체가능");
+
+			} else if (sendMoney > myMoney) {
+				System.out.println("이체불가");
+			}
+		} else {
+			System.out.println("없는 계좌");
+		}
+
 		System.out.println("myMoney = " + myMoney + "원");
 		System.out.println("yourMoney = " + yourMoney + "원");
 
 	}
-
 }
--- src/basic/step02/Test09.java
+++ src/basic/step02/Test09.java
@@ -18,9 +18,40 @@ public class Test09 {
 		int balance = 30000; // 입금시 증가 , 출금시 감소
 		int account = 11111; // 로그인시 사용
 		int password = 1234; // 로그인시 사용
+		
+		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("===MEGA ATM===");
 		System.out.println("1.로그인 2.종료");
+		int sel = sc.nextInt();
+		if(sel == 1) {
+			System.out.println("id 및 pw 입력: ");
+			int acc = sc.nextInt();
+			int pw = sc.nextInt();
+			if(acc == account && pw == password) {
+				
+				System.out.println("안녕하세요 " + account+"님");
+				System.out.println("1.입금  2.출금  3.조회");
+				int sel2 = sc.nextInt();
+				if(sel2 == 1) {
+					System.out.println("입금할 금액: ");
+					int inCach = sc.nextInt();
+					
+					
+				}else if(sel2 == 2) {
+					
+				}else if(sel2 == 3) {
+					
+				}
+				
+			}else {
+				System.out.println("존재하지 않는 계정");
+			}
+			
+
+		}else {
+			System.out.println("종료");
+		}
 
 	}
 
