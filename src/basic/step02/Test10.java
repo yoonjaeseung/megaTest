bac91f420c3fce425fe7feae260600788aaa8767
berno <berno579@gmail.com>
2020-07-06 00:01:15 +0900

    Day02 review 0705

--- src/basic/step02/Test10.java
+++ src/basic/step02/Test10.java
@@ -1,4 +1,5 @@
 package basic.step02;
+
 /*
  * # 가운데 숫자 맞추기 게임
  * 1. 150~250 사이의 랜덤 숫자 저장
@@ -6,7 +7,7 @@ package basic.step02;
  * 예)
  * 		249		: 4
  */
-
+//다시
 public class Test10 {
 
 	public static void main(String[] args) {
