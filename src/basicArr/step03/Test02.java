daa68a30c32e6ad74766c021a99acf0b08dc5511
SAMSUNG <SAMSUNG@YOON-FLEX>
2020-07-26 18:38:25 +0900

    update reviews 0726

--- src/basicArr/step03/Test02.java
+++ src/basicArr/step03/Test02.java
@@ -2,7 +2,7 @@ package basicArr.step03;
 
 /*
  * # 영화관 좌석예매
- * 1. 사용자로부터 좌석번호(index)를 입력받아 예매하는 시스템이다.
+ * 1. 사용자로부터 좌석번호(index)를 입력받아 예매하는 시스템이다(1~7 입력)
  * 2. 예매가 완료되면 해당 좌석 값을 1로 변경한다.
  * 3. 이미 예매가 완료된 좌석은 재구매할 수 없다.
  * 4. 한 좌석당 예매 가격은 12000원이다.
@@ -22,7 +22,7 @@ package basicArr.step03;
  * ----------------------
  * 매출액 : 24000원
  */
-//다시
+//9분
 import java.util.Scanner;
 
 public class Test02 {
@@ -31,9 +31,20 @@ public class Test02 {
 		Scanner sc = new Scanner(System.in);
 
 		int[] seat = new int[7];
-
+		int cnt = 0;
 		boolean run = true;
 		while (run) {
+
+			// start
+			for (int i = 0; i < 7; i++) {
+				if (seat[i] == 0) {
+					System.out.print("[ ]");
+				} else {
+					System.out.print("[" + seat[i] + "]");
+				}
+			}
+			System.out.println();
+
 			System.out.println("=메가 영화관=");
 			System.out.println("1.좌석예매");
 			System.out.println("2.종료");
@@ -42,8 +53,21 @@ public class Test02 {
 			int sel = sc.nextInt();
 
 			if (sel == 1) {
+
+				System.out.print("좌석 선택[1~7]: ");
+				int idx = sc.nextInt();
+				idx -= 1;
+				if (seat[idx] == 0) {
+					seat[idx] = 1;
+					cnt++;
+				} else if (seat[idx] == 1) {
+					System.out.println("이미 예매된 자리입니다.");
+				}
+
 			} else if (sel == 2) {
 				run = false;
+				int tot = cnt * 12000;
+				System.out.println("총매출: " + tot);
 			}
 		}
 
--- src/basicArr/step04/Test01.java
+++ src/basicArr/step04/Test01.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
  * [문제]복습
  * 
  */
-
+//0545 52/ 52
 public class Test01 {
 
 	public static void main(String[] args) {
@@ -17,17 +17,64 @@ public class Test01 {
 
 		// 문제1) 학번을 입력하면 점수 출력
 		// 문제2) 점수를 입력하면 학번 출력
-		int score[] = { 1001, 20, 43, 1002, 54, 2, 1003, 76, 6 };
+
+		// start
+		// System.out.println("문제1,2>>>");
+		// boolean run = true;
+		// while (run) {
+		// System.out.println("==성적조회==");
+		// System.out.println("1.학번 조회 / 2.점수 조회");
+		// int sel = sc.nextInt();
+		// if (sel == 1) {
+		// System.out.print("학번 입력: ");
+		// int hakbun = sc.nextInt();
+		//
+		// for (int i = 0; i < 6; i++) {
+		// if (arr[i] == hakbun) {
+		// System.out.println("점수: " + arr[i + 1]);
+		// }
+		// }
+		// } else if (sel == 2) {
+		// System.out.print("점수 입력: ");
+		// int score = sc.nextInt();
+		//
+		// for (int i = 0; i < 6; i++) {
+		// if (arr[i] == score) {
+		// System.out.println("점수: " + arr[i - 1]);
+		// }
+		// }
+		// }
+		// }
+
+		int scores[] = { 1001, 20, 43, 1002, 54, 2, 1003, 76, 6 };
 
 		// 학번과 국어 수학 점수이다
 		// 예) 1001 == (20 , 43)
 		// 1002 == (54 , 2)
 		// 1003 == (76 , 6)
-		
+
 		// 문제3) 점수의 합을 입력하면 번호 출력
 		// 예) 63 ==> 1001
 		// 예) 56 ==> 1002
 		// 예) 82 ==> 1003
+		int sum = 0;
+		int idx = 0;
+		// System.out.print("점수의 합 입력: ");
+		// int ans = sc.nextInt();
+		int[] temp = new int[6];
+		for (int i = 0; i < 9; i++) {
+			if (scores[i] / 1000 == 0) {
+				temp[i] = scores[i];
+				System.out.print(temp[i] + " ");
+			}
+
+			// if (ans == sum) {
+			// idx = i - 1;
+			// }
+
+		}
+		System.out.println();
+		System.out.println("학번: " + scores[idx]);
 
 	}
 
